3febb5924a1448c193619076a79fb41377a7053d
Sarah Carlson <sbcarls@gmail.com>
2021-03-03 20:27:58 -0500

    updated mongo connection

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -24,13 +24,6 @@ mongoose.connect(
     useFindAndModify: false
   }
 );
-
-// mongoose.connect("mongodb://localhost/budget" || process.env.MONGODB_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useCreateIndex: true,
-//       useFindAndModify: false
-// });
 
 // routes
 app.use(require("./routes/api.js"));
